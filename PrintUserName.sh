1b18e33ed8176284adec70ab98e67f0102a5f6b1
Suorna <Suorna@github.com>
2017-05-31 10:27:17 -0700

    first Commit

---
+++ PrintUserName.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+echo "Enter your Name: "
+read name
+
+echo -e  "\n !! Greetings $name !! "
+
+echo -e  "\n Please Press Enter to Continue"
+read
+
+echo -e "\n !! Have a Good Day $name !! \n"
+
