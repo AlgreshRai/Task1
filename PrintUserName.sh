07c32e7f545623a9cc671df1ffc3bec60e1d6fe6
futuremaharjan <falcket@gmail.com>
2017-06-10 11:31:19 +0545

    enter first and last name

--- PrintUserName.sh
+++ PrintUserName.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 clear
-echo "Enter your Name: "
-read name
+echo "Enter your first name: "
+read fname
+echo "Enter your last name"
+read lname
 
-echo -e  "\n !! Greetings $name !! "
+echo -e  "\n !! Greetings $fname $lname!! "
 
 echo -e  "\n Please Press Enter to Continue"
 read
 
-echo -e "\n !! Have a Good Day $name !! \n"
+echo -e "\n !! Have a Good Day $fname $lname !! \n"
 
